8a63b1cd9fbd3029598741675bc21cbc33d865c4
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-25 10:10:58 -0500

    completed show function for individual hoots;

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -26,7 +26,16 @@ router.get('/', async (req, res) => {
         const hoots = await Hoot.find({}).populate('author').sort({ createdAt: 'desc' });
         res.status(200).json(hoots);
     } catch (err) {
-        res.status(500).json(error);
+        res.status(500).json(err);
+    }
+})
+
+router.get('/:hootId', async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate('author');
+        res.status(200).json(hoot)
+    } catch (err) {
+        res.status(500).json(err);
     }
 })
 
